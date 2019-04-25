6c435a46adb80ecd15ef357ad7ddd00ea0167b7f
Benoit Sagols <benoit.sagols@gmail.com>
2019-04-25 13:14:00 +0700

    hotfix

--- index.js
+++ index.js
@@ -7,32 +7,32 @@ const { assign } = Object;
 const styles = {};
 
 for (let i = 1; i < 10; i++) {
-	const col = dial('column', i);
-	const row = dial('row', i);
+  const col = dial('column', i);
+  const row = dial('row', i);
 
-	assign(styles, {
-		[`Col${i}`]: { ...col, flex: 1 },
-		[`Row${i}`]: { ...row, flex: 1 },
-	});
+  assign(styles, {
+    [`Col${i}`]: { ...col, flex: 1 },
+    [`Row${i}`]: { ...row, flex: 1 },
+  });
 }
 
 const sheets = StyleSheet.create(styles);
 
 const makeView = (name, sheet = null) => ({
-	[name]: ({ style, ...rest }) => (
-		<View {...rest} style={[sheet || sheets[name], style]} />
-	),
+  [name]: ({ style, ...rest }) => (
+    <View {...rest} style={[sheet || sheets[name], style]} />
+  ),
 });
 
 const Views = {};
 
 for (let i = 1; i < 10; i++) {
-	assign(Views, makeView(`Col${i}`));
-	assign(Views, makeView(`Row${i}`));
+  assign(Views, makeView(`Col${i}`));
+  assign(Views, makeView(`Row${i}`));
 }
 
 // Aliases
-assign(Views, makeView('Col', sheets.Col1);
-assign(Views, makeView('Row', sheets.Row1);
+assign(Views, makeView('Col', sheets.Col1));
+assign(Views, makeView('Row', sheets.Row1));
 
 export default Views;
