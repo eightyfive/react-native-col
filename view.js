bf02e8a9c18caa66ece8baa78dcc575fbe02d3dc
Benoit SAGOLS <benoit.sagols@gmail.com>
2021-01-03 23:23:17 +0100

    Add x property (flex) support

--- view.js
+++ view.js
@@ -3,16 +3,32 @@ import { View as RNView } from 'react-native';
 
 import partition from './space';
 
-export default function View(props) {
+const $ = {};
+
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].forEach((flex) => {
+  $[`x${flex}`] = { flex };
+});
+
+function View({ x, ...props }) {
   if (View.sizes.length) {
     const { style, ...rest } = props;
 
     const [view, styles] = partition(rest, View.sizes);
 
-    return <RNView {...view} style={[...styles, style]} />;
+    let flex = false;
+
+    if (typeof x !== 'undefined') {
+      flex = x === true ? 1 : x;
+    }
+
+    return (
+      <RNView {...view} style={[...styles, flex && $[`x${flex}`], style]} />
+    );
   }
 
   return <RNView {...props} />;
 }
 
 View.sizes = [];
+
+export default View;
